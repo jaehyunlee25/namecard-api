176ddf65d17f418f5741dc4dfc30769371eb30ad
jaehyunlee <jaehyunlee25@gmail.com>
2023-02-06 05:32:31 +0900

    'bomun'

--- sql/getLog.sql
+++ sql/getLog.sql
@@ -4,6 +4,6 @@ select
 from LOG 
 where 
     DATE(created_at) = '${date}'
-    and device_id = '${device_id}'
-    and golf_club_id = '${golf_club_id}'
+    and device_id like '%${device_id}%'
+    and golf_club_id like '%${golf_club_id}%'
 order by created_at asc;
