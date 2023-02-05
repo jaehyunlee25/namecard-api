550d328bd7b5a76908906aa4f9b5d436d965fbb5
jae hyun lee <jaehyunlee25@gmail.com>
2023-02-05 08:46:40 +0000

    bomun script

--- script/search_core/smartku.js
+++ script/search_core/smartku.js
@@ -1,20 +1,41 @@
 function mneCall(date, callback) {
   const param = {
-    golfrestype: "real",
-    schDate: date,
-    usrmemcd: "11",
-    toDay: date + "01",
-    calnum: "1",
+    clickTdId: "",
+    clickTdClass: "",
+    workMonth: date,
+    workDate: date + "01",
+    bookgDate: "",
+    bookgTime: "",
+    bookgCourse: "",
+    searchTime: "",
+    selfTYn: "",
+    temp001: "",
+    bookgComment: "",
+    memberCd: "11",
+    temp007: "",
+    agencyReservationYn: "N",
+    certSeq: "",
+    contactName: "",
+    contactTel1: "010",
+    contactTel2: "",
+    contactTel3: "",
+    certNoChk: "",
+    agencyBookgName: "",
+    agencyHp1: "010",
+    agencyHp2: "",
+    agencyHp3: "",
+    agreeYn: "Y",
   };
-  post("real_calendar_ajax_view.asp", param, {}, (data) => {
+  post("/reservation/ajax/golfCalendar", param, {}, (data) => {
     const ifr = document.createElement("div");
     ifr.innerHTML = data;
 
-    const as = Array.from(ifr.gcn("cal_live"));
-    as.forEach((a) => {
-      const param = a.attr("href").inparen();
-      const [elDate] = param;
-      dates.push([elDate, 0]);
+    const attr = "onclick";
+    const els = ifr.gba(attr, "clickCal(", true);
+    els.forEach((el) => {
+      const [, sign, fulldate, opt] = el.attr(attr).inparen();
+      if (opt != "OPEN") return;
+      dates.push([fulldate, sign]);
     });
     callback();
   });
@@ -22,28 +43,56 @@ function mneCall(date, callback) {
 
 /* <============line_div==========> */
 function mneCallDetail(arrDate) {
-  const [date] = arrDate;
+  const fCall = { post, get };
+  const [date, sign] = arrDate;
+  const addr = "/reservation/ajax/golfTimeList";
+  const method = "post";
   const param = {
-    golfrestype: "real",
-    courseid: "0",
-    usrmemcd: "11",
-    pointdate: date,
-    openyn: "1",
-    dategbn: "3",
-    choice_time: "00",
-    cssncourseum: "",
-    inputtype: "I",
+    clickTdId: "A" + date,
+    clickTdClass: "",
+    workMonth: date.ct(2),
+    workDate: date,
+    bookgDate: "",
+    bookgTime: "",
+    bookgCourse: "ALL",
+    searchTime: "",
+    selfTYn: "",
+    temp001: "",
+    bookgComment: "",
+    memberCd: "11",
+    temp007: "",
+    agencyReservationYn: "N",
+    certSeq: "",
+    contactName: "",
+    contactTel1: "010",
+    contactTel2: "",
+    contactTel3: "",
+    certNoChk: "",
+    agencyBookgName: "",
+    agencyHp1: "010",
+    agencyHp2: "",
+    agencyHp3: "",
+    agreeYn: "Y",
   };
-  post("real_timelist_ajax_list.asp", param, {}, (data) => {
-    const ifr = document.createElement("div");
+  const dictCourse = {
+    1: "혼솔",
+    2: "미쁨",
+    3: "바른",
+  };
+
+  fCall[method](addr, param, {}, (data) => {
+    const ifr = doc.clm("div");
     ifr.innerHTML = data;
-    const trs = ifr.getElementsByTagName("tbody")[0].getElementsByTagName("tr");
-    const obTeams = {};
-    Array.from(trs).forEach((tr, i) => {
-      const course = tr.children[1].innerHTML;
-      const time = tr.children[2].innerHTML;
-      const fee_normal = tr.children[4].innerHTML.replace(/\,/g, "") * 1;
-      const fee_discount = tr.children[4].innerHTML.replace(/\,/g, "") * 1;
+
+    const attr = "onclick";
+    const els = ifr.gba(attr, "golfConfirm(", true);
+    Array.from(els).forEach((el) => {
+      let [date, time, course, , , hole, fee] = el.attr(attr).inparen(true);
+      hole = hole.ct(1);
+      course = dictCourse[course];
+      fee = fee.rm(",") * 1;
+      fee_normal = fee;
+      fee_discount = fee;
 
       golf_schedule.push({
         golf_club_id: clubId,
@@ -54,7 +103,7 @@ function mneCallDetail(arrDate) {
         persons: "",
         fee_normal,
         fee_discount,
-        others: "18홀",
+        others: hole + "홀",
       });
     });
     procDate();
