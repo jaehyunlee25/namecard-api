166f3a50c2490164853c7e588453e071665a13df
jaehyunlee <jaehyunlee25@gmail.com>
2023-02-14 14:55:22 +0900

    'SCRIPT_ERROR_IN_AJAX_CALLBACK'

--- opSearch.js
+++ opSearch.js
@@ -8,13 +8,27 @@ String.prototype.ct = function (num) {
 String.prototype.has = function (str) {
   return this.indexOf(str) != -1;
 };
+String.prototype.regex = function (re) {
+  return re.exec(this);
+};
 
 const clubName = "gumi";
-const pathDict = "./script/search_core/";
+const pathDict = "./script/search_dict/";
 
 const files = fs.readdirSync(pathDict);
 files.forEach((file) => {
   const full = pathDict + file;
   const con = fs.readFileSync(full, "utf-8");
-  log(con);
+  try {
+    const jsn = JSON.parse(con);
+    jsn.forEach(([title, addr, func], i) => {
+      if (addr.regex(/\s/g)) {
+        log(file);
+        log(title, addr, func);
+      }
+    });
+  } catch (e) {
+    log(file);
+    log(e);
+  }
 });
