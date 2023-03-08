a6b03b9a8201fc9dfa9c5aae9b421f8a6c634acb
jae hyun lee <jaehyunlee25@gmail.com>
2023-03-08 15:48:02 +0000

    gunsan

--- script/backup/search_core_1678290431752_gunsan.js
+++ script/backup/search_core_1678290431752_gunsan.js
@@ -2,32 +2,14 @@ function mneCall(date, callback) {
   intvEl = window["calBody"];
   ${mneCallCommon}
   function exec() {
-    const str = (date + "01").datify();
-    const param = {};
-    Array.from(aspnetForm.elements).forEach(
-      (el) => (param[el.name] = el.value)
-    );
-    param["ctl00$ContentPlaceHolder1$scManager"] =
-      "ctl00$ContentPlaceHolder1$scManager|ctl00$ContentPlaceHolder1$btnUp";
-    param["__EVENTTARGET"] = "ctl00$ContentPlaceHolder1$btnUp";
-    param["__EVENTARGUMENT"] = "";
-    param["ctl00$ContentPlaceHolder1$htbArgs"] =
-      "CALENDAR|" + str + "|" + str + "|date";
-    param["__ASYNCPOST"] = true;
-    post("/Mobile/", param, {}, (data) => {
-      const ifr = doc.clm("div");
-      ifr.innerHTML = data;
-
-      const attr = "href";
-      const els = ifr.gba(attr, "javascript:Update('LIST", true);
-      Array.from(els).forEach((el) => {
-        if (el.nm(1).className.indexOf("possible") == -1) return;
-        const [str] = el.attr(attr).inparen();
-        const [, , date, sign] = str.split("|");
-        dates.push([date.rm("-"), sign, str]);
-      });
-      callback();
+    const els = doc.gba("href", "javascript:Update('LIST", true);
+    Array.from(els).forEach((el) => {
+      if (el.nm(1).className.indexOf("possible") == -1) return;
+      const [str] = el.attr("href").inparen();
+      const [, , date, sign] = str.split("|");
+      dates.push([date.rm("-"), sign, str]);
     });
+    callback();
   }
 }
 
@@ -89,5 +71,6 @@ function mneCallDetail(arrDate) {
 
 /* <============line_div==========> */
 mneCall(thisdate, () => {
+  doc.gcn("arw-next")[0].click();
   mneCall(nextdate, procDate);
 });
