3b204ecf4d8efbe854f1b849cebc89d1a11d5581
jae hyun lee <jaehyunlee25@gmail.com>
2023-03-08 16:21:53 +0000

    renew

--- script/backup/search_core_1678291579568_gunsan.js
+++ script/backup/search_core_1678291579568_gunsan.js
@@ -20,7 +20,6 @@ function mneCall(date, callback) {
 
       const attr = "href";
       const els = ifr.gba(attr, "javascript:Update('LIST", true);
-      EXTZLOG("search", ["els", JSON.stringify(param)].join(", "), logPrm);
       Array.from(els).forEach((el) => {
         if (el.nm(1).className.indexOf("possible") == -1) return;
         const [str] = el.attr(attr).inparen();
@@ -31,6 +30,7 @@ function mneCall(date, callback) {
     });
   }
 }
+
 
 /* <============line_div==========> */
 function mneCallDetail(arrDate) {
