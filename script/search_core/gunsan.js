ff8b4ba356eb66360d1b02317f651a4866a55432
jaehyunlee <jaehyunlee25@gmail.com>
2023-03-09 00:59:09 +0900

    gunsan

--- script/search_core/gunsan.js
+++ script/search_core/gunsan.js
@@ -1,29 +1,36 @@
 function mneCall(date, callback) {
-  const str = (date + "01").datify();
-  const param = {};
-  Array.from(aspnetForm.elements).forEach((el) => (param[el.name] = el.value));
-  param["ctl00$ContentPlaceHolder1$scManager"] =
-    "ctl00$ContentPlaceHolder1$scManager|ctl00$ContentPlaceHolder1$btnUp";
-  param["__EVENTTARGET"] = "ctl00$ContentPlaceHolder1$btnUp";
-  param["__EVENTARGUMENT"] = "";
-  param["ctl00$ContentPlaceHolder1$htbArgs"] =
-    "CALENDAR|" + str + "|" + str + "|date";
-  param["__ASYNCPOST"] = true;
-  post("/Mobile/", param, {}, (data) => {
-    const ifr = doc.clm("div");
-    ifr.innerHTML = data;
+  intvEl = window["calBody"];
+  ${mneCallCommon}
+  function exec() {
+    const str = (date + "01").datify();
+    const param = {};
+    Array.from(aspnetForm.elements).forEach(
+      (el) => (param[el.name] = el.value)
+    );
+    param["ctl00$ContentPlaceHolder1$scManager"] =
+      "ctl00$ContentPlaceHolder1$scManager|ctl00$ContentPlaceHolder1$btnUp";
+    param["__EVENTTARGET"] = "ctl00$ContentPlaceHolder1$btnUp";
+    param["__EVENTARGUMENT"] = "";
+    param["ctl00$ContentPlaceHolder1$htbArgs"] =
+      "CALENDAR|" + str + "|" + str + "|date";
+    param["__ASYNCPOST"] = true;
+    post("/Mobile/", param, {}, (data) => {
+      const ifr = doc.clm("div");
+      ifr.innerHTML = data;
 
-    const attr = "href";
-    const els = ifr.gba(attr, "javascript:Update('LIST", true);
-    Array.from(els).forEach((el) => {
-      if (el.nm(1).className.indexOf("possible") == -1) return;
-      const [str] = el.attr(attr).inparen();
-      const [, , date, sign] = str.split("|");
-      dates.push([date.rm("-"), sign, str]);
+      const attr = "href";
+      const els = ifr.gba(attr, "javascript:Update('LIST", true);
+      Array.from(els).forEach((el) => {
+        if (el.nm(1).className.indexOf("possible") == -1) return;
+        const [str] = el.attr(attr).inparen();
+        const [, , date, sign] = str.split("|");
+        dates.push([date.rm("-"), sign, str]);
+      });
+      callback();
     });
-    callback();
-  });
+  }
 }
+
 
 /* <============line_div==========> */
 function mneCallDetail(arrDate) {
