deba94c8bd96dce137348784e89cd84121b8439b
jaehyunlee <jaehyunlee25@gmail.com>
2023-03-20 16:06:13 +0900

    'SCRIPT_ERROR_IN_AJAX_CALLBACK'

--- teezzim.js
+++ teezzim.js
@@ -281,9 +281,26 @@ function procPost(request, response, data) {
     "sql/getGolfNews.sql".gfdp({}).query((err, rows, fields) => {
       objResp = stdSQLProc(err, rows);
       if (objResp.type == "okay") {
+        const res = {};
         objResp.data.forEach((ob) => {
-          ob.link_name = golfLinks[ob.link_name].name;
+          const eng_id = ob.link_name;
+          const name = golfLinks[ob.link_name].name;
+          const id = golfLinks[ob.link_name].id;
+          if (!res[eng_id])
+            res[eng_id] = {
+              eng_id,
+              name,
+              id,
+              content: [],
+            };
+          ob.link_name = "[" + name + "]";
+          res[eng_id].content.push(ob);
         });
+        const result = { news: [] };
+        Object.entries(res).forEach(([key, val]) => {
+          result.news.push(val);
+        });
+        objResp.data = result;
       }
       response.write(JSON.stringify(objResp));
       response.end();
