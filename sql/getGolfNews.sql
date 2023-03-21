ae6c2a51c3c135656e5a4f545d991d94965c9cdd
jaehyunlee <jaehyunlee25@gmail.com>
2023-03-21 15:34:07 +0900

    'SCRIPT_ERROR_IN_AJAX_CALLBACK'

--- sql/getGolfNews.sql
+++ sql/getGolfNews.sql
@@ -1,10 +1,13 @@
-select
+SELECT	
+	ROUND,
+	NUMBER,	
     address link_address,
-    eng_id link_name,
+    golf_news.eng_id link_name,
     content link_content,
     datetime link_datetime
 from
     golf_news
+JOIN golf_link ON golf_link.eng_id = golf_news.eng_id
 where
     round = (select max(round) from golf_news)
-ORDER BY number;
+ORDER BY NUMBER, description;
