a8849ef24773e4e5e403bc730bac968045d4ddd4
jaehyunlee <jaehyunlee25@gmail.com>
2023-03-21 15:22:37 +0900

    'SCRIPT_ERROR_IN_AJAX_CALLBACK'

--- teezzim.js
+++ teezzim.js
@@ -310,14 +310,15 @@ function procPost(request, response, data) {
     const { news } = data;
     const vls = [];
     news.forEach((ob) => {
-      const {
+      let {
         link_round: round,
         link_number: number,
         link_address: address,
         link_name: eng_id,
-        link_content: content.replace(/"/g, '\\"'),
+        link_content: content,
         link_datetime: datetime,
       } = ob;
+      content = content.replace(/"/g, '\\"');
       const tpl = `(uuid(), "${round}", "${number}", "${address}", "${eng_id}", "${content}", "${datetime}", now(), now())`;
       log(tpl);
       vls.push(tpl);
