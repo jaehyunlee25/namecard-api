e7f6a640d38e758a3e4ef95780940c0876dfa58b
jaehyunlee <jaehyunlee25@gmail.com>
2023-03-30 18:08:05 +0900

    'SCRIPT_ERROR_IN_AJAX_CALLBACK'

--- sql/getGolfNews.sql
+++ sql/getGolfNews.sql
@@ -9,5 +9,5 @@ from
     golf_news
 JOIN golf_link ON golf_link.eng_id = golf_news.eng_id
 where
-    round = (select round from golf_news order by round desc LIMIT 1 OFFSET 1)
+    round = (select distinct(ROUND) from golf_news order by round desc LIMIT 1 OFFSET 1)
 ORDER BY NUMBER, description;
