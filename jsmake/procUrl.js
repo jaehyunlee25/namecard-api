ea8c05a3dedde2fd65c3ccd268cdbda865165690
jaehyunlee <jaehyunlee25@gmail.com>
2023-04-03 15:23:51 +0900

    'SCRIPT_ERROR_IN_AJAX_CALLBACK'

--- jsmake/procUrl.js
+++ jsmake/procUrl.js
@@ -507,26 +507,27 @@ if (reqUrl == "/dummy") {
       data: list,
     };
 } else if (reqUrl == "/getLog" ) {
-    "sql/getLog.sql".gfdp(data).query((err, rows, fields) => {
-      if (err) {
-        objResp = {
-          type: "error",
-          data: err,
-        };
-      } else {
-        objResp = {
-          type: "okay",
-          data: rows,
-        };
-      }
-      response.write(JSON.stringify(objResp));
-      response.end();
-    });
-    getLog((err, rows, fields) => {
+"sql/getLog.sql".gfdp(data).query((err, rows, fields) => {
+  if (err) {
+    objResp = {
+      type: "error",
+      data: err,
+    };
+  } else {
+    objResp = {
+      type: "okay",
+      data: rows,
+    };
+  }
+  response.write(JSON.stringify(objResp));
+  response.end();
+});
+/* getLog((err, rows, fields) => {
       objResp = { resultCode: 200, message: "okay", data: rows };
       response.write(JSON.stringify(objResp));
       response.end();
-    });
+    }); */
+
 } else if (reqUrl == "/getLogClubList" ) {
     "sql/getLogClubList.sql".gfdp(data).query((err, rows, fields) => {
       if (err) {
