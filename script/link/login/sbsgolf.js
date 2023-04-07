fd06db447f9a67f41e373e683f890b0688c6f035
jaehyunlee <jaehyunlee25@gmail.com>
2023-04-07 14:56:21 +0900

    'SCRIPT_ERROR_IN_AJAX_CALLBACK'

--- script/link/login/sbsgolf.js
+++ script/link/login/sbsgolf.js
@@ -1,29 +1,32 @@
-setTimeout(() => {
-  log("check", doc.gcn("gmembers_link_login").length);
-  if (doc.gcn("gmembers_link_login").length > 0) {
-    log("check", doc.gcn("gmembers_link_login")[0].str());
-    if (doc.gcn("gmembers_link_login")[0].str() == "로그인") {
-      log("check", "enter");
-      doc.gcn("gmembers_link_login")[0].click();
+const raw = location.href;
+if (raw.indexOf("login") != -1) {
+  setTimeout(() => {
+    log("check", doc.gcn("gmembers_link_login").length);
+    if (doc.gcn("gmembers_link_login").length > 0) {
+      log("check", doc.gcn("gmembers_link_login")[0].str());
+      if (doc.gcn("gmembers_link_login")[0].str() == "로그인") {
+        log("check", "enter");
+        doc.gcn("gmembers_link_login")[0].click();
+        return;
+      }
+    }
+  }, 2000);
+} else {
+  var tLoginCount = 0;
+  log("tLoginCount", tLoginCount);
+  const tLogin = setInterval(timeraction, 1000);
+  timeraction();
+  function timeraction() {
+    if (!window["loginId"]) {
+      tLoginCount++;
+      log("tLoginCount", tLoginCount);
+      if (tLoginCount > 4) clearInterval(tLogin);
       return;
     }
+    clearInterval(tLogin);
+    if (precheck()) return;
+    window["loginId"].value = "${login_id}";
+    window["loginPw"].value = "${login_password}";
+    window["loginBtn"].click();
   }
-}, 2000);
-/* 
-var tLoginCount = 0;
-log("tLoginCount", tLoginCount);
-const tLogin = setInterval(timeraction, 1000);
-timeraction();
-function timeraction() {
-  if (!window["loginId"]) {
-    tLoginCount++;
-    log("tLoginCount", tLoginCount);
-    if (tLoginCount > 4) clearInterval(tLogin);
-    return;
-  }
-  clearInterval(tLogin);
-  if (precheck()) return;
-  window["loginId"].value = "${login_id}";
-  window["loginPw"].value = "${login_password}";
-  window["loginBtn"].click();
-} */
+}
