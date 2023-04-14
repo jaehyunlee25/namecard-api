6f5b8df5a6a32ff5d1b66895d709f9382153e63c
jaehyunlee <jaehyunlee25@gmail.com>
2023-04-14 13:28:07 +0900

    'SCRIPT_ERROR_IN_AJAX_CALLBACK'

--- jsmake/urlmake/url/delGolfFashion.js
+++ jsmake/urlmake/url/delGolfFashion.js
@@ -1,9 +1,4 @@
 "sql/delGolfFashion.sql".gfdp(data).query((err, rows, fields) => {
-  const addr =
-    "https://mnemosynesolutions.co.kr/app/project/editor_source_golf/img/upload/";
-  rows.forEach((row) => {
-    row.thumbnail = addr + row.thumbnail;
-  });
   objResp = stdSQLProc(err, rows);
   response.write(JSON.stringify(objResp));
   response.end();
