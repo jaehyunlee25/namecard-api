f0dd265104adeb2df08c5aef7b4ee335c79c150a
jaehyunlee <jaehyunlee25@gmail.com>
2023-04-14 13:25:04 +0900

    'SCRIPT_ERROR_IN_AJAX_CALLBACK'

--- teezzim.js
+++ teezzim.js
@@ -597,6 +597,11 @@ const { eventId } = data;
 
 } else if (reqUrl == "/delGolfFashion" ) {
 "sql/delGolfFashion.sql".gfdp(data).query((err, rows, fields) => {
+  const addr =
+    "https://mnemosynesolutions.co.kr/app/project/editor_source_golf/img/upload/";
+  rows.forEach((row) => {
+    row.thumbnail = addr + row.thumbnail;
+  });
   objResp = stdSQLProc(err, rows);
   response.write(JSON.stringify(objResp));
   response.end();
