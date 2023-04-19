cbb948a6813079a0e775623d58f05f00d3676cbb
jae hyun lee <jaehyunlee25@gmail.com>
2023-04-19 21:16:00 +0900

    'renew'

--- script/login/shilla_KMH.js
+++ script/login/shilla_KMH.js
@@ -3,15 +3,22 @@ EXTZLOG("login", "tLoginCount: " + tLoginCount);
 const tLogin = setInterval(timeraction, 1000);
 timeraction();
 function timeraction() {
+  EXTZLOG("login", "timeraction_0");
   if (!window["usrId"]) {
     tLoginCount++;
     EXTZLOG("login", "tLoginCount: " + tLoginCount);
     if (tLoginCount > 4) clearInterval(tLogin);
     return;
   }
+  EXTZLOG("login", "timeraction_1");
   clearInterval(tLogin);
+  EXTZLOG("login", "timeraction_2");
   if (precheck()) return;
+  EXTZLOG("login", "timeraction_3");
   usrId.value = "${login_id}";
+  EXTZLOG("login", "timeraction_4");
   usrPwd.value = "${login_password}";
+  EXTZLOG("login", "timeraction_5");
   fnLogin.click();
+  EXTZLOG("login", "timeraction_6");
 }
