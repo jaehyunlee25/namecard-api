e038ed2ee1ddf2027f846f3b27df1efb0f021288
jae hyun lee <jaehyunlee25@gmail.com>
2023-11-18 16:31:25 +0900

    'renew'

--- namecard.js
+++ namecard.js
@@ -825,6 +825,7 @@ function procPost(req, response, data, files) {
 function babybell(req, response, data) {
   const { step } = data;
   const objResp = { result: `단계 ${step}, 전송되었습니다.` };
+  return;
 
   const kakao_token =
     "qPuTePcSFE2hpExuv6kgR9aY9DOL7pVOmoMKKiWOAAABi-BF2ReIenTzhLqDRQ";
